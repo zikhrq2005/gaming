0b8a436ea233d8d820ee57e75a15f52a377f5419
zikhrq2005 <hazmm9257@gmail.com>
2025-10-21 11:18:16 +0300

    animation

--- My project (1)/Assets/NewBehaviourScript.cs
+++ My project (1)/Assets/NewBehaviourScript.cs
@@ -13,10 +13,13 @@ public class NewBehaviourScript : MonoBehaviour
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     private bool grounded;
+
+    
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,6 +32,7 @@ public class NewBehaviourScript : MonoBehaviour
         if (Input.GetKey(L))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
             if (GetComponent<SpriteRenderer>()!= null)
             {
                 GetComponent<SpriteRenderer>().flipX = true;
@@ -42,10 +46,20 @@ public class NewBehaviourScript : MonoBehaviour
                 GetComponent<SpriteRenderer>().flipX = false;
             }
         }
+
+
+        anim.SetFloat("Speed",Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+
     }
     void jump()
     {
         GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHight);
+
+
+        anim.SetFloat("Height", GetComponent<Rigidbody2D>().velocity.y);
+        anim.SetBool("Grounded", grounded);
+
+
     }
     void FixedUpdate()
     {
